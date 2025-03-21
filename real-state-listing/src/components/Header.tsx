4c22976270f8e399237af7a9be4148ed664e6616
TiagoEsdras <tiagoesdrasvieira@gmail.com>
2025-03-21 16:54:18 -0300

    Created header

--- real-state-listing/src/components/Header.tsx
+++ real-state-listing/src/components/Header.tsx
@@ -1,7 +1,29 @@
-//import React from 'react';
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="py-6 mb-12 bg-dark-blue px-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to='/' className="text-off-white font-bold">
+          Real State
+        </Link>
+        <div className="flex items-center gap-4">
+          <Link
+            to=''
+            className="py-3 px-4 text-off-white rounded-lg"
+          >
+            Login
+          </Link>
+          <Link
+            to=''
+            className="py-3 px-4 rounded-lg bg-gold text-dark-blue"
+          >
+            Register
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
