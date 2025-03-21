5b29b48838f36341399e408b4b7c31710950648d
TiagoEsdras <tiagoesdrasvieira@gmail.com>
2025-03-21 20:19:00 -0300

    Created filterDropdown

--- real-state-listing/src/components/FilterDropdown.tsx
+++ real-state-listing/src/components/FilterDropdown.tsx
@@ -1,7 +1,37 @@
-//import React from 'react';
+import { useState } from "react";
+import { RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
+import { Menu, MenuButton } from "@headlessui/react";
 
-const FilterDropdown = () => {
-  return <div>FilterDropdown</div>;
+interface FilterDropdownProps {
+  icon: React.ReactNode;
+  title: string
+}
+
+const FilterDropdown: React.FC<FilterDropdownProps> = ({ icon, title }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Menu as='div' className='w-full lg:max-w-[296px] cursor-pointer relative'>
+      <MenuButton onClick={() => setIsOpen(!isOpen)} className='flex h-[64px] items-center px-[18px] border border-dark-gray rounded-lg w-full text-left'>
+        {icon}
+        <div>
+          <div className="text-[15px] font-medium leading-tight text-dark-blue">
+            {title}
+          </div>
+          <div className="text-[13px] text-dark-gray">
+            Select...
+          </div>
+        </div>
+        {
+          isOpen ? (
+            <RiArrowUpSLine className="text-2xl text-dark-gray ml-auto" />
+          ) : (
+            <RiArrowDownSLine className="text-2xl text-dark-gray ml-auto" />
+          )
+        }
+      </MenuButton>
+    </Menu>
+  );
 };
 
 export default FilterDropdown;
--- real-state-listing/src/components/Search.tsx
+++ real-state-listing/src/components/Search.tsx
@@ -1,12 +1,14 @@
 import { RiSearch2Line } from "react-icons/ri";
 import FilterDropdown from "./FilterDropdown";
 import FilterRange from "./FilterRange";
+import { FaBath, FaBed, FaCar } from "react-icons/fa";
+
 const Search = () => {
   return (
-    <div className="px-[30px] py-6 max-w-100 lg:max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between gap-4 lg:gap-x-3 relative lg:-top-4 lg:shadow-2xl bg-[#8c827367] lg:backdrop-blur rounded-lg">
-      <FilterDropdown />
-      <FilterDropdown />
-      <FilterDropdown />
+    <div className="px-[30px] py-6 max-w-100 lg:max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between gap-4 lg:gap-x-3 relative lg:-top-4 lg:shadow-2xl bg-[#eae7e3a4] lg:backdrop-blur rounded-lg">
+      <FilterDropdown icon={<FaBed className="text-2xl mr-[18px] text-dark-blue" />} title="Bedrooms" />
+      <FilterDropdown icon={<FaBath className="text-2xl mr-[18px] text-dark-blue" />} title="Bathrooms" />
+      <FilterDropdown icon={<FaCar className="text-2xl mr-[18px] text-dark-blue" />} title="Parking" />
       <FilterRange />
       <button className="bg-dark-blue w-full lg:max-w-[160px] h-16 rounded-lg flex justify-center items-center text-off-white text-lg">
         <RiSearch2Line />
