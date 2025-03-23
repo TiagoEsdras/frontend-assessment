54e63464b7349595be2dd9aba21d9ed1b2d452b7
TiagoEsdras <tiagoesdrasvieira@gmail.com>
2025-03-23 09:29:11 -0300

    Add message when listing is empty

--- real-state-listing/src/components/Listing.tsx
+++ real-state-listing/src/components/Listing.tsx
@@ -8,22 +8,30 @@ const Listing = () => {
   const context = useContext(ListingContext);
   const { listings, loading } = context!;
 
+  if (loading) {
+    return (
+      <Loading />
+    );
+  }
+
+  if (listings.length == 0) {
+    return (
+      <div className="text-center text-3xl text-dark-gray mt-48">No results found. Please adjust your filters and try again.</div>
+    );
+  }
+
   return (
     <section id="listing" className="mb-20">
       <div className="container mx-auto">
-        {loading ? (
-          <Loading />
-        ) : (
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-14">
-            {listings.map((listing, index) => {
-              return (
-                <Link to={`listing/${listing.id}`} key={index}>
-                  <ListingCard listing={listing} />
-                </Link>
-              );
-            })}
-          </div>
-        )}
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-14">
+          {listings.map((listing, index) => {
+            return (
+              <Link to={`listing/${listing.id}`} key={index}>
+                <ListingCard listing={listing} />
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
